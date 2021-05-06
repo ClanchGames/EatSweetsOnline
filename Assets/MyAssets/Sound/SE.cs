62d1f9756da0dd44df7cfd5c407f5d2c23d52ef9
ClanchGames <clanchgames@gmail.com>
2021-05-06 17:43:58 +0900

    効果音再生機能追加

--- Assets/MyAssets/Sound/SE.cs
+++ Assets/MyAssets/Sound/SE.cs
@@ -4,22 +4,63 @@ using UnityEngine;
 
 public class SE : MonoBehaviour
 {
-    [NamedArray(new string[]{
-       "GameClear",
-       "GameOver",
-       "pokopoko",
-        })]
-    public AudioClip[] AudioClips;
+    static public SE se;
 
+
+    public AudioClip GameOverSE;
+    public AudioClip GameClearSE;
+    public AudioClip PopSE;
+    AudioSource audioSource;
+
+    [SerializeField]
+    float delay = 0.06f;
+
+    float realtimelastplay = 0;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        if (se == null)
+        {
+            se = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine("TestSE");
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private IEnumerator TestSE()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            PlaySE(PopSE);
+        }
+    }
+
+    /// <summary>
+    /// AudioClipÇçƒê∂Ç∑ÇÈÉÅÉ\ÉbÉh
+    /// </summary>
+    /// <param name="audioClip"></param>
+    public void PlaySE(AudioClip audioClip)
+    {
+        if (Time.realtimeSinceStartup - realtimelastplay > delay)
+        {
+            realtimelastplay = Time.realtimeSinceStartup;
+            audioSource.PlayOneShot(audioClip);
+        }
     }
 }
